3d973754c3c143f0be54949250c39cf6b017f5a9
Odilon Enrique <odilonegduarte@gmail.com>
2024-02-07 20:03:42 -0300

    fixed paginate home

--- src/hooks/useQueryPokemonPage.ts
+++ src/hooks/useQueryPokemonPage.ts
@@ -7,7 +7,7 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 export function useQueryPokemonPage() {
   const [page, setPage] = useState(1);
   const [limit, setLimit] = useState(30);
-  const [totalPages, setTotalPages] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
   const navigate = useNavigate();
   const searchParams = useSearchParams();
@@ -16,11 +16,13 @@ export function useQueryPokemonPage() {
     const offset = (page - 1) * limit;
     const { data } = await API.get(`/pokemon?limit=${limit}&offset=${offset}`);
 
-    const pokemonPromises = data.results.map(async (pokemon: { url: string }) => {
-      const response = await fetch(pokemon.url);
-      const data = await response.json();
-      return data;
-    });
+    const pokemonPromises = data.results.map(
+      async (pokemon: { url: string }) => {
+        const response = await fetch(pokemon.url);
+        const data = await response.json();
+        return data;
+      }
+    );
 
     const pokemonData = await Promise.all(pokemonPromises);
 
@@ -48,7 +50,21 @@ export function useQueryPokemonPage() {
     const pageQuery = Number(searchParams[0].get("page"));
 
     setPage(pageQuery || 1);
-  }, [searchParams]);
+
+    if (totalPages > 0) {
+      if (pageQuery > totalPages) {
+        navigate(`?page=${totalPages}`);
+        setPage(totalPages);
+        return;
+      }
+
+      if (pageQuery < 1) {
+        navigate(`?page=1`);
+        setPage(1);
+        return;
+      }
+    }
+  }, [searchParams, page, totalPages, navigate]);
 
   const query = useQuery({
     queryKey: ["getPokemonPage", page, limit],
