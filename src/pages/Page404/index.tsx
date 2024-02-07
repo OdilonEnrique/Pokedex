915075ecc2df65b98faf72ce38ed223698d6aa12
Odilon Enrique <odilonegduarte@gmail.com>
2024-02-07 20:20:20 -0300

    add page 404

--- src/pages/Page404/index.tsx
+++ src/pages/Page404/index.tsx
@@ -1,9 +1,13 @@
 import { Container } from "./style";
+import page404 from "../../assets/page404.png";
+import { useNavigate } from "react-router-dom";
 
 export function Page404() {
+  const navigate = useNavigate();
   return (
     <Container>
-      <h1>Page 404</h1>
+      <img src={page404} alt="page404" />
+      <button onClick={() => navigate("/")}>Voltar</button>
     </Container>
   );
 }
--- src/pages/Page404/style.ts
+++ src/pages/Page404/style.ts
@@ -1,9 +1,44 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  min-height: 100vh;
+  display: grid;
+  place-content: center;
+  height: 100vh;
 
-  h1 {
-    font-size: 1.4rem;
+  img {
+    margin-top: 2.4rem;
+    width: 32rem;
+    box-shadow: 1px 1px 2px 1px #0002;
+  }
+
+  button {
+    background: ${({ theme }) => theme.colors.danger};
+    color: ${({ theme }) => theme.colors.light};
+    box-shadow: 1px 1px 1px #0004;
+    border-radius: 3.2rem;
+    font-size: 1.2rem;
+    font-weight: 700;
+    padding: 1rem;
+    width: 30%;
+    margin: 1.6rem auto 0;
+
+    &:hover {
+      cursor: pointer;
+      filter: brightness(1.05);
+    }
+  }
+
+  @media (min-width: 768px) {
+    img {
+      margin-top: 4.8rem;
+      width: 56rem;
+    }
+
+    button {
+      font-size: 1.6rem;
+      padding: 1.2rem;
+      width: 25%;
+      margin: 2.4rem auto 0;
+    }
   }
 `;
